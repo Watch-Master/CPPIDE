957c9d612942bdfd47b8552f151158390db2a5e9
Watch-Master <169016273+Watch-Master@users.noreply.github.com>
2026-05-21 19:21:23 +0530

    Delete build directory

--- build/CMakeFiles/CodePlayground.dir/CodePlayground_autogen/EWIEGA46WW/qrc_qml.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CodePlayground.dir/CodePlayground_autogen/EWIEGA46WW/qrc_qml.cpp.o: \
- /home/sohamwm/Desktop/PY_and_HTML_Apps/CPPIDE/build/CodePlayground_autogen/EWIEGA46WW/qrc_qml.cpp \
- /usr/include/stdc-predef.h
